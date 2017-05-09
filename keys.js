493ab8403d812a0b14c4928040c2479cc88a6e77
ElmasSayed <elmassayed@gmail.com>
2017-05-09 00:05:01 -0700

    hw turned in

--- keys.js
+++ keys.js
@@ -7,28 +7,7 @@ exports.twitterKeys = {
 }
 
 
-// Application Settings
-// Keep the "Consumer Secret" a secret. This key should never be human-readable in your application.
-// Consumer Key (API Key)	wGJoD6Q6CjH1D3cAFEaYS5uoy
-// Consumer Secret (API Secret)	R95ENJ48huRS0CHNziILUjmLHppAsVzHtZsVQHx6op9Q4Od4P4
-// Access Level	Read and write (modify app permissions)
-// Owner	bubble23979368
-// Owner ID	860285914534117376
-
-// our Access Token
-// This access token can be used to make API requests on your own account's behalf. Do not share your access token secret with anyone.
-// Access Token	860285914534117376-cmBuCvRj1U3iGHJ4WrCo96KpRGWB6xW
-// Access Token Secret	B4pQ9m0Oa121rW7VVNMXp32iHFCK76ljGifO4H2RYXuME
-// Access Level	Read and write
-// Owner	bubble23979368
-// Owner ID	860285914534117376
-
 exports.spotifyKeys = {
   consumer_key: '00f05b39fe29448e851660be0995c2d9',
   consumer_secret: '03b5c9f0f60e496ab742219404639002',
 }
-
-// Client ID
-// 00f05b39fe29448e851660be0995c2d9
-// Client Secret
-// 03b5c9f0f60e496ab742219404639002
--- liri.js
+++ liri.js
@@ -1,31 +1,17 @@
-// console.log("786");
 
-// var allKeys = require("./keys.js");
-// console.log(allKeys.twitterKeys.consumer_key);
+var fs = require("fs");
+var allKeys = require('./keys.js');
 var request = require("request");
 var twitter = require('twitter');
 var spotify = require('spotify');
 
-
-
+//arg0 and arg1 us already taken by node
 var api = process.argv[2];
 var param = process.argv[3];
-// console.log(api);
-// console.log(param);
-
-// validating if arguments are ok.
-// if (api !== "my-tweets" &&  api !== "spotify-this-song" &&  api !== "movie-this" &&  api !== "do-what-it-says" &&  api !== "giphy-this"){
-// 	console.log("Second parameter API is invalid.");
-// 	return
-// }
-
-if(param == null){
-	console.log("Please specify 3rd parameter")
-	return
-}
+var param2 = process.argv[4];
 
 if (api == "my-tweets"){
-	showMytweets(param);
+	showMyTweets(param);
 }else if(api == "spotify-this-song"){
  	showSpotifySongs(param)
 }else if(api == "movie-this"){
@@ -38,9 +24,10 @@ if (api == "my-tweets"){
 	console.log("Second parameter API is invalid.");
 	return
 }
-
-// Functions
-// Giphy
+// ------------------------------------------------------------------------------------------------------------------
+// 													Functions
+// ------------------------------------------------------------------------------------------------------------------
+// GIPHY
 function showGiphys(param){
 		var endpoint = "https://api.giphy.com/v1/gifs/search"; 
 		var querystring = "?q=" + param + "&limit=10&api_key=dc6zaTOxFJmzC"; 
@@ -52,20 +39,62 @@ function showGiphys(param){
 			}
 		});
 }
+// ------------------------------------------------------------------------------------------------------------------
+//OMDB
+function showMovies(param){
+	
+	if (param == null){
+		param = "Mr. Nobody";
+	}
+  	
+	var endpoint = "http://www.omdbapi.com/";
+	var querystring = "?t=" + param + "&y=&plot=short&r=json"; 
+	var queryURL = endpoint + querystring;
+	// console.log(queryURL);
+	request(queryURL, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			body = JSON.parse(body);
+			//console.log(JSON.parse(body));
+			var Title = body.Title;
+			var Year = body.Year;
+			var Rating = body.imdbRating;
+			var Country = body.Country;
+			var Language = body.Language;
+			var Plot = body.Plot;
+			var Actors = body.Actors;
+			var Rotten = body.Ratings[1].Value;
 
+	  		console.log("-------------------------------------------------------");
+			console.log("Title of the movie: " + Title.toUpperCase());
+			console.log("* Year the movie came out: " + Year);
+			console.log("* IMDB Rating of the movie: " + Rating);
+			console.log("* Country where the movie was produced: " + Country);
+			console.log("* Language of the movie: " + Language);
+			console.log("* Plot of the movie: " + Plot);
+			console.log("* Actors in the movie: " + Actors);
+			console.log("* Rotten Tomatoes: " + Rotten);
+	  		console.log("-------------------------------------------------------");
+	  		if(param === "Mr. Nobody"){
+	  			console.log("If you haven't watched 'Mr. Nobody,' then you should: <http://www.imdb.com/title/tt0485947/>  It's on Netflix!")
+   	  		}
+		}
+	});
+}
 // -------------------------------------------------------------------------------------------------
-
-// Functions
 //SPOTIFY
 function showSpotifySongs(param){
+
+    if (param == null){
+		param = "The Sign";
+	}
+
 	spotify.search({ type: 'track', query: param }, function(err, data) {
 	    if ( err ) {
 	        console.log('Error occurred: ' + err);
 	        return;
 	    }
-	 
-	   // console.log(JSON.stringify(data,null,4));
-  	for( var i = 0; i < data.tracks.items.length; i++){
+
+	   	for( var i = 0; i < data.tracks.items.length; i++){
 	  		var artist = data.tracks.items[i].album.artists[0].name;
 	  		var song = data.tracks.items[i].name;
 	  		var previewLink = data.tracks.items[i].preview_url;
@@ -80,53 +109,41 @@ function showSpotifySongs(param){
 }
 // --------------------------------------------------------------------------------------------------------
 
-// // Functions
-// //twitter
-// function twitter(param){
-// 	twitter.search({ type: 'track', query: param }, function(err, data) {
-// 	    if ( err ) {
-// 	        console.log('Error occurred: ' + err);
-// 	        return;
-// 	    }
+//twitter
+function showMyTweets(param){
+
+	var client = new twitter(allKeys.twitterKeys);
 	 
-// 	   // console.log(JSON.stringify(data,null,4));
-//   	for( var i = 0; i < data.tracks.items.length; i++){
-// 	  		var artist = data.tracks.items[i].album.artists[0].name;
-// 	  		var song = data.tracks.items[i].name;
-// 	  		var previewLink = data.tracks.items[i].preview_url;
-// 	  		var album = data.tracks.items[i].album.name;
-// 	  		console.log("Artist: " + artist);
-// 	  		console.log("Song: " + song);
-// 	  		console.log("Preview link : " + previewLink);
-// 	  		console.log("Album: " + album);
-// 	  		console.log("-------------------------------------------------------");
-// 	   	};
-// 	});
-// }
+	var params = {screen_name: 'bubble23979368'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (!error && response.statusCode === 200) {
+			for(var i=0; i < tweets.length; i++){
+				console.log("text: " + tweets[i].text);
+				console.log("-------------------------------------------------------");
+				console.log("created at: " + tweets[i].created_at);
+			}
+		}
+	});
+}	
+// ----------------------------------------------------------------------------------------------------------
 
+// do-what-it-says
 
-// --------------------------------------------------------------------------------------------------
+function showDoAsItSays(param){
 
-// Functions
-//SPOTIFY
-function showSpotifySongs(param){
-	spotify.search({ type: 'track', query: param }, function(err, data) {
-	    if ( err ) {
-	        console.log('Error occurred: ' + err);
-	        return;
-	    }
-	 
-	   // console.log(JSON.stringify(data,null,4));
-  	for( var i = 0; i < data.tracks.items.length; i++){
-	  		var artist = data.tracks.items[i].album.artists[0].name;
-	  		var song = data.tracks.items[i].name;
-	  		var previewLink = data.tracks.items[i].preview_url;
-	  		var album = data.tracks.items[i].album.name;
-	  		console.log("Artist: " + artist);
-	  		console.log("Song: " + song);
-	  		console.log("Preview link : " + previewLink);
-	  		console.log("Album: " + album);
-	  		console.log("-------------------------------------------------------");
-	   	};
+	fs.writeFile("random.txt", param, function(err){
+		if(err){
+			return console.log(err);
+		}else{
+			fs.readFile("./random.txt", function(err,data){
+				if(err){
+					return console.log(err);
+				}else{
+					showSpotifySongs(data);
+				}				
+			});
+		}
 	});
 }
+
+// --------------------------------------------------------------------------------------
